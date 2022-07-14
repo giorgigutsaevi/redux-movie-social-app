2a54ad6578059fe5f610349c8d288933af7c1085
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2022-07-14 11:25:02 +0100

    configuring movie sagas and setting up rootSagas to be added in configureStore.js

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,8 +1,16 @@
 import { combineReducers } from "redux";
+import { all, fork } from "redux-saga/effects";
 import movieReducer from "./movies/reducer";
+import moviesSaga from "./movies/sagas";
 
 const rootReducer  = combineReducers({
 	movies: movieReducer
 })
 
 export default rootReducer;
+
+export function* rootSaga(){
+	yield all([
+		fork(moviesSaga),
+	])
+}
--- client/src/store/movies/sagas.js
+++ client/src/store/movies/sagas.js
@@ -1,9 +1,33 @@
+import { call, put, takeEvery, all, fork, take } from 'redux-saga/effects'
+import { callApi } from '../../utils/api';
+import { fetchSuccess, fetchError } from './actions';
+import { FETCH_REQUEST } from './types';
+
+const MOVIE_DB_API_KEY = process.env.REACT_APP_API_KEY
+const COURSE_API_URL = `https://api.themoviedb.org/3/movie/550?api_key=${MOVIE_DB_API_KEY}`
 
 // WORKER
 
-
+// FETCH - saga
+function* handleFetch() {
+	try {
+		const response = yield call(callApi, COURSE_API_URL, 'GET');
+		yield put(fetchSuccess(response))
+	} catch (error) {
+		yield put(fetchError(error))
+	}
+}
 
 // WATCHER
+function* watchHandleFetch() {
+	yield takeEvery(FETCH_REQUEST, handleFetch)
+}
 
+// grouping all feature-wise sagas and exporting
+function* moviesSaga() {
+	yield all([
+		fork(watchHandleFetch)
+	])
+}
 
-// rootSagas
+export default moviesSaga;
