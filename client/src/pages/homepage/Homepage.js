08bc207f9556b37dcecc6a753027cf33dfb26f36
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2022-08-01 11:55:23 +0100

    refactored homepage component

--- client/src/pages/homepage/Homepage.js
+++ client/src/pages/homepage/Homepage.js
@@ -7,27 +7,36 @@ import { Link } from 'react-router-dom';
 const Homepage = () => {
 	return (
 		<div className='homepage'>
-			<h1 className='homepage-title'>Your favorite movies and TV shows gathered in one place.</h1>
-			<h4 className='homepage-sub-title'>Enjoy hundrees of your favourite movies, TV shows, documentaries and cartoons provided by the MovieDB API and carefuly designed and curated by me. </h4>
-			<div className='homepage-socials'>
-				<Link to="#" className='homepage-socials-icons'>
-					<RiTwitterLine
-						size={35}
-					/>
-				</Link>
-				<Link to='#' className='homepage-socials-icons'>
-					<RiInstagramLine
-						size={35}
-					/>
-				</Link>
-				<Link to='#' className='homepage-socials-icons'>
-					<RiFacebookBoxLine
-						size={35}
-					/>
-				</Link>
-			</div>
-			<div className='homepage-call-to-action'>
-				<button className='watchlist-button'>My Watchlist</button>
+			<div className='container-fluid'>
+				<div className='row'>
+					<div className='col-md homepage-column'>
+						<h1 className='homepage-title'>Your favorite movies and TV shows gathered in one place.</h1>
+
+						<h4 className='homepage-sub-title'>Enjoy hundrees of your favourite movies, TV shows, documentaries and cartoons provided by the MovieDB API and carefuly designed and curated by me. </h4>
+
+						<div className='homepage-socials'>
+							<Link to="#" className='homepage-socials-icons'>
+								<RiTwitterLine
+									size={35}
+								/>
+							</Link>
+							<Link to='#' className='homepage-socials-icons'>
+								<RiInstagramLine
+									size={35}
+								/>
+							</Link>
+							<Link to='#' className='homepage-socials-icons'>
+								<RiFacebookBoxLine
+									size={35}
+								/>
+							</Link>
+						</div>
+
+						<div className='homepage-call-to-action'>
+							<button className='watchlist-button'>My Watchlist</button>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	)
